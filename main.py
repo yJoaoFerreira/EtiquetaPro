7e485737bd2a33ddb9c1e90cefaf169e304f7e7f
João Ferreira <yferreirajoao@gmail.com>
2024-10-11 13:40:08 -0300

    Pandas importado, leitura básica de Excel adicionada.

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# Este software deve cumprir as seguintes etapas para se considerar "completo":
+import pandas as pd
 
-# 1. Obter o diretório de um arquivo Excel;
-# 2. Ler um arquivo Excel do diretório selecionado;
-# 3. Obter informações do Excel no geral;
-# 4. Manipular essas informações (usar regex se necessário);
-# 5. Deixar as informações no molde de impressão;
-# 6. Gerar um arquivo em PDF para impressão já nos moldes de etiqueta;
-# 7. Estabelecer uma conexão da impressora com o programa;
-# 8. Conectar-se com a impressora serial selecionada;
-# 9. Enviar o PDF gerado para a impressora selecionada;
-# 10. Imprimir o PDF enviado através do programa.
+pd.__version__
+
+pd.read_excel('Teste.xlsx')
+print("Script executado com sucesso!")
